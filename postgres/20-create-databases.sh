bbfbee37f92e053a0158677d7f87cffe61197524
ordu1453 <1muzaffer.4ali.5ordu3@gmail.com>
2025-12-15 18:44:10 +0300

    small fixes

--- postgres/20-create-databases.sh
+++ postgres/20-create-databases.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
+echo "=== Running database init script for variant v4 ==="
+
+
 # TODO для создания баз прописать свой вариант
-export VARIANT="<variant>"
+export VARIANT="4"
 export SCRIPT_PATH=/docker-entrypoint-initdb.d/
 export PGPASSWORD=postgres
-psql -f "$SCRIPT_PATH/scripts/db-$VARIANT.sql"
+psql -f "$SCRIPT_PATH/scripts/db-v$VARIANT.sql"
